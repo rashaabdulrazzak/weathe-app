711ddcbd1ea5de69341eaaeaaab986db65cae83f
rashaabdulrazzak <rasha.abdulrazzak.com>
2020-10-15 21:49:31 +0300

    partial view

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const path = require('path')
 const express = require('express')
+// we require hbsto be able to use parials 
+const hbs = require('hbs')
+
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
-// to be able to use handle bar 
-app.set('view engine', 'hbs');
+const viewPath = path.join(__dirname,'../templates/views')
+const partialPath =  path.join(__dirname,'../templates/partials')
+/*set up handle bar engine and views location and partials  */
+app.set('view engine', 'hbs'); 
+app.set('views',viewPath)
+hbs.registerPartials(partialPath)
+
+// set up static location 
 app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res)=>{
@@ -21,6 +30,8 @@ app.get('/about',(req,res)=>{
 })
 app.get('/help',(req,res)=>{
     res.render('help',{
+        title:'weather app',
+        name:'rasha',
         message:'that is ahelp'
     })
 })
