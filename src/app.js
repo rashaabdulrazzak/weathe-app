1132f2fc43edb02a1cdbf6e3fc0c21947465eb8b
rashaabdulrazzak <rasha.abdulrazzak.com>
2020-10-15 18:47:45 +0300

    serving static asset

--- src/app.js
+++ src/app.js
@@ -1,25 +1,19 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
-// send html
-app.get('',(req,res)=>{
-     res.send('<h1>hello express</h1>')
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
 })
-// send json
-app.get('/weather',(req,res)=>{
-   res.send({
-       location : 'Boston',
-       teprature:'25'
-   })
-})
-// send array 
-app.get('/help',(req,res)=>{
-    res.send([{name : 'rasha'},{name : 'rasha'}])
-})
-app.get('/about',(req,res)=>{
-    res.send('<h1>about page </h1>')
- })
-// start the server 
-app.listen(3000,()=>{
-    console.log('server is set up to port 3000')
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+    console.log(publicDirectoryPath)
 })
